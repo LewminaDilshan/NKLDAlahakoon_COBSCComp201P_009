21acece0fa78c63d4744fd5206210f9a6491155c
Lewmina Dilshan <lewmina96@gmail.com>
2021-11-07 09:20:48 +0530

    Initial Commit

---
+++ NKLDAlahakoon_COBSCComp201P_009/NKLDAlahakoon_COBSCComp201P_009App.swift
@@ -0,0 +1,17 @@
+//
+//  NKLDAlahakoon_COBSCComp201P_009App.swift
+//  NKLDAlahakoon_COBSCComp201P_009
+//
+//  Created by Lewmina Dilshan on 2021-11-07.
+//
+
+import SwiftUI
+
+@main
+struct NKLDAlahakoon_COBSCComp201P_009App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
